fd08f215d63d26e2e10ad91dde39d5cb804f6e00
Faisal Iqbal <109624462+FaisalDeveloper113@users.noreply.github.com>
2022-07-25 00:36:45 +0500

    Helpful Comments Added

--- 3_Ballot.sol
+++ 3_Ballot.sol
@@ -3,14 +3,20 @@ pragma solidity 0.8.15;
 
 contract Inbox 
 {
+    // Solidity Automaticaly Creates Function of Public Variables
     string public message;
 
+    // Code to Write the constructor function
+    // Keyword memory in parameters
     constructor (string memory initialmessage) public {
         message = initialmessage;
     }
     function setMessage(string memory newMessage) public {
         message = newMessage;
     }
+    // We dont need this function if message is puvlic
+    // If you deploy contract messgae button will already be there 
+    // we dont need a get message function
     function getMessage() public view returns (string memory) {
         return message;
     }
